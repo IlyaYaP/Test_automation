b002d3ba8f7222bea24d4f9fbe5aeba57a601a11
Ilya <socspec@yandex.ru>
2023-04-06 17:40:37 +0300

    1

--- test_product_page.py
+++ test_product_page.py
@@ -26,6 +26,7 @@ import time
 #     page.should_not_be_success_message()
 #     page.should_not_be_success_message_dis()
 
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
@@ -34,3 +35,19 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.guest_can_add_product_to_basket()
     time.sleep(3)
     page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    time.sleep(5)
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.guest_can_add_product_to_basket()
+    page.should_not_be_success_message_dis()
